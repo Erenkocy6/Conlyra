91cf009cd8d3179216eaba4980a2285db983bb53
Erenkocy6 <eren.kocyigit@mni.thm.de>
2026-07-08 16:48:56 +0200

    Load CONLYRA director editorial styles

--- NovaIQ/app/layout.tsx
+++ NovaIQ/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Cormorant_Garamond, Poppins } from "next/font/google";
 import "./globals.css";
+import "./director-patch.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
